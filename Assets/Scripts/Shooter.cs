95ccf047e1739fa49864aff3910d7a911306da62
aitorortunio <43756956+aitorortunio@users.noreply.github.com>
2020-03-21 15:43:46 -0300

    Improvements

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -8,7 +8,7 @@ public class Shooter : MonoBehaviour
 
     public void Fire()
     {
-        Vector2 newVector = new Vector2(transform.position.x + 0.9f, transform.position.y - 0.1f);
-        Instantiate(projectile,newVector, transform.rotation);
+        Vector2 projectilePosition = new Vector2(transform.position.x + 0.37f, transform.position.y - 0.1f);
+        Instantiate(projectile, projectilePosition, transform.rotation);
     }
 }
