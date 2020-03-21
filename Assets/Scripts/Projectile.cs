6a116536ce330cb5e84acec67f2057abd5c2d6e8
aitorortunio <43756956+aitorortunio@users.noreply.github.com>
2020-03-21 16:32:40 -0300

    (some) Health & Damage

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    [Range(0f, 10f)] [SerializeField] float speed = 3f;
+    [Range(0f, 10f)] [SerializeField] float speed = 3.5f;
+    [SerializeField] float damage = 50;
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        var health = collider.GetComponent<Health>();
+        health.DealDamage(damage);
+        Destroy(gameObject);
     }
 }
