77562f987aba8b24a155d743dcef122164d8bcc4
aitorortunio <43756956+aitorortunio@users.noreply.github.com>
2020-03-21 00:43:43 -0300

    Houston, we have a flying arrow

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-    [SerializeField] GameObject projectile;
+    [SerializeField] Projectile projectile;
 
     public void Fire()
     {
-
+        Vector2 newVector = new Vector2(transform.position.x + 0.9f, transform.position.y - 0.1f);
+        Instantiate(projectile,newVector, transform.rotation);
     }
 }
